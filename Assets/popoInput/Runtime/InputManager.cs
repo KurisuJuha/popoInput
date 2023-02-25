5a22b4cc217746e28f6d7cef12105a6a88a766f0
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 19:29:56 +0900

    コンストラクタでclientID、secretID、clientsを生成

--- Assets/popoInput/Runtime/InputManager.cs
+++ Assets/popoInput/Runtime/InputManager.cs
@@ -9,14 +9,17 @@ namespace JuhaKurisu.PopoTools.InputSystem
         public ReadOnlyCollection<Client> clients { get; private set; }
         public readonly int playerCount;
 
-        public InputManager(int playerCount, Func<byte[]> GetInput)
         private readonly Func<byte[]> GetInput;
         private readonly Guid clientID;
         private readonly Guid secretID;
 
+        public InputManager(Guid clientID, Guid secretID, int playerCount, Func<byte[]> GetInput)
         {
+            this.clientID = clientID;
+            this.secretID = secretID;
             this.playerCount = playerCount;
             this.GetInput = GetInput;
+            clients = new(new Client[0]);
         }
 
         public void UpdateInputs(byte[] bytes)
