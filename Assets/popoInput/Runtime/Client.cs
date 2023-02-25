9de6ba5487641e51857a08f982cf942d9a21ca52
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 19:26:17 +0900

    byte配列から全て生成するのではなく分けて生成するようにした

--- Assets/popoInput/Runtime/Client.cs
+++ Assets/popoInput/Runtime/Client.cs
@@ -1,29 +1,16 @@
 using System;
-using System.Linq;
-using JuhaKurisu.PopoTools.ByteSerializer;
 
 namespace JuhaKurisu.PopoTools.InputSystem
 {
     public class Client
     {
-        public readonly Guid id;
+        public readonly Guid clientID;
         public readonly Input input;
 
-        public Client(byte[] bytes)
+        public Client(Guid clientID, byte[] inputBytes)
         {
-            DataReader reader = new(bytes);
-            id = reader.ReadGuid();
-            input = new Input(reader.ReadBytes());
-        }
-
-        public byte[] Serialize()
-        {
-            DataWriter writer = new DataWriter();
-
-            writer.Append(id);
-            writer.Append(input.Serialize());
-
-            return writer.bytes.ToArray();
+            this.clientID = clientID;
+            this.input = new Input(inputBytes);
         }
     }
 }
